3af99399e9326d188a29211eb81d8da81e6f2ee6
양종빈 <>
2019-10-23 16:58:19 +0900

    @

--- src/reducers/reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import authReducer from "./authReducer";
-import loginReducer from "./loginReducer";
-
-export default combineReducers({
-  authReducerInfo: authReducer,
-  loginReducerInfo: loginReducer
-});
